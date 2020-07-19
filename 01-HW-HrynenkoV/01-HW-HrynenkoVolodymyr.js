737326601d4f71f3e442221dbf7751412aed46a0
Volodymyr <80937561312q@gmail.com>
2020-07-20 00:45:37 +0300

    01-HW-HrynenkoVolodymyr added

---
+++ 01-HW-HrynenkoV/01-HW-HrynenkoVolodymyr.js
@@ -0,0 +1,70 @@
+const car = 15.678;
+console.log(car);
+
+const bus = 123.965;
+console.log(bus);
+
+const train = 90.2345;
+console.log(train);
+
+const oplataklient = 500;
+console.log(oplataklient);
+
+const maxPrice = Math.max(car, bus, train);
+console.log(maxPrice); // 1 максимальне число
+
+const minPrice = Math.min(car, bus, train);
+console.log(minPrice); // 2 мінімальне число
+
+const sumaAllTransport = car + bus + train;
+console.log(sumaAllTransport); // 3 сума чисел
+
+const floorAllTransport = Math.floor(car) + Math.floor(bus) + Math.floor(train);
+console.log(floorAllTransport); // 4 округлення
+
+const sumaRounding = sumaAllTransport.toFixed(2);
+console.log(sumaRounding); // 5 округлення до сотень
+
+const yesornot = floorAllTransport % 2 === 0 ?'Yes' : 'No';
+console.log(yesornot); // 6 парне чи непарне число
+
+const rest = oplataklient - sumaAllTransport;
+console.log(rest); // 7 решта суми з 500
+
+const  averageValue = (sumaAllTransport / 3);
+console.log(averageValue); // 8 середня сума з 3-х чисел
+
+const rounding = averageValue.toFixed(2);
+console.log(rounding); // 8 округлення до 2-го знака
+
+const discount = (Math.random()*100);
+console.log(discount); // 9 знижка
+
+const discountRounding = (Math.floor(discount).toFixed(2));
+console.log(discountRounding); // 9 округлена знижка
+
+const profit = (sumaAllTransport - (sumaAllTransport*discountRounding/100));
+console.log(profit); // 9 прибуток
+
+const cost = sumaAllTransport / 2;
+console.log(cost); // 9 собівартість
+
+const happyEnd = cost - profit;
+console.log(happyEnd); // 9 чистий прибуток
+
+const results = 'Car: ${car}
+Bus: ${bus}
+Train: ${train}
+Максимальне число: ${maxPrice}
+Мінімальне число: ${minPrice}
+Сума всіх чисел - ${sumaAllTransport}
+Округлення в меншу сторону: ${floorAllTransport}
+Сума округлена до сотень: ${sumaRounding}
+Булове значення округлене в меншу сторону так чи ні: ${yesornot ? 'Yes' : 'No'}
+Сума решти з 500 грн.: ${rest}
+Середнє значення цін оруглене до сотих: ${rounding}
+Округлена знижка: ${discountRounding}
+Прибуток: ${profit}
+Собівартість: ${cost}
+Чистий прибуток: ${happyEnd}';
+console.log(results);
