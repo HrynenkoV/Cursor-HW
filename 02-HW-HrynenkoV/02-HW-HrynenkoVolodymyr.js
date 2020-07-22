b7b4d2da6a25026bb3a39d3a8c6fb509d7c6e5e3
Volodymyr <80937561312q@gmail.com>
2020-07-23 02:20:39 +0300

    02-HW-HrynenkoV added

--- 02-HW-HrynenkoV/02-HW-HrynenkoVolodymyr.js
+++ 02-HW-HrynenkoV/02-HW-HrynenkoVolodymyr.js
@@ -1,16 +1,34 @@
 let firstNumber;
 let secondNumber;
 
-firstNumber = Number(prompt ("Будь ласка напишіть перше число"));
+firstNumber = Number(prompt ("Будь ласка напишіть перше ціле число"));
 while (isNaN(firstNumber) || !Number.isInteger(firstNumber) || firstNumber==="") {
   firstNumber = Number(prompt ("Потрібно написати ЦІЛЕ число"))
 }
 console.log(firstNumber);
 
-secondNumber = Number(prompt ("Будь ласка напишіть друге число, але більше за перше"));
-while (isNaN(secondNumber) || !Number.isInteger(secondNumber) || secondNumber==="" || firstNumber>=secondNumber) {
+secondNumber = Number(prompt ("Будь ласка напишіть друге ціле число, але більше за перше"));
+
+while (isNaN(secondNumber)) {
   secondNumber = Number(prompt ("Потрібно написати ЦІЛЕ число"))
 }
+
+while (!Number.isInteger(secondNumber)) {
+  secondNumber = Number(prompt ("Потрібно написати ЦІЛЕ число"))
+}
+
+while (secondNumber==="") {
+  secondNumber = Number(prompt ("Потрібно написати ЦІЛЕ число"))
+}
+
+while (firstNumber>=secondNumber) {
+  secondNumber = Number(prompt ("Потрібно написати ціле число більше за перше"))
+}
+
+/* while (isNaN(secondNumber) || !Number.isInteger(secondNumber) || secondNumber==="" || firstNumber>=secondNumber) {
+  secondNumber = Number(prompt ("Потрібно написати ЦІЛЕ число"))
+} */
+
 console.log(secondNumber);
 
 const missNumber = confirm("Будемо пропускати парні числа?");
