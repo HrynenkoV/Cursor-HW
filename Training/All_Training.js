80175f5a9a52854f2cb5b7347f339039066e096d
Volodymyr <80937561312q@gmail.com>
2020-07-30 23:19:08 +0300

    04-HW added

--- Training/All_Training.js
+++ Training/All_Training.js
@@ -240,3 +240,142 @@ console.log(HrynenkoV.toString());
 console.log(HrynenkoV.toString().length);
 console.log(HrynenkoV.toString().length.toString()); */
 
+/* const Vova = 26;
+const Vika = 25;
+const age = [ Vova, Vika];
+console.log(age); */
+
+/* const Hrynenko = ['Vova', 'Vika', 'Gena', 'Nataly'];
+const name = +prompt(`pls write name`);
+
+console.log(Hrynenko[name]); */
+
+/* const arr = [[26, 25], [31, 29]];
+console.log('arr: ', arr);
+
+arr.push(200, 300);
+console.log('arr: ', arr); */
+
+
+/* const arr2 = new Array(['Vova', 'Vika'], ['Gena', 'Nataly']);
+console.log('arr2: ', arr2); */
+
+/* arr2.shift();
+console.log('arr2: ', arr2); */
+
+
+/* arr2.pop();
+console.log('arr2: ', arr2); */
+
+/* arr2.unshift(['Slava' , 'Zheka']);
+console.log('arr2: ', arr2); */
+
+
+/* const nameVova = [[26, 'Vova', 25, 'Vika'] , [31, 'Gena' , 29,  'Nataly']];
+console.log('nameVova: ', nameVova); */
+
+/* const a = parseInt(prompt `push number`);
+
+const b = parseInt(prompt `push number`);
+
+const c = parseInt(prompt `push number`);
+console.log(a, b, c);
+
+if (a >= b && a >= c) {
+  console.log(`A bigger`, a);
+} else if (b >= c) {
+  console.log(`B bigger`, b);
+} else {
+  console.log(`C bigger`, c); */
+
+  /* const Akva = ['Vova', 'Yra', 'Sasha', 'Roma'];
+
+for (const index in Akva){
+  console.log('Akvamarin: ', Akva[index]);
+  } */
+
+
+  
+
+/* function biggerNumber() {
+  const numbaers = [];
+const maxNumbers = 3
+for (let a = 0; a < maxNumbers; a++) {
+  numbaers.push(parseInt(prompt `push number`));
+  }
+
+  let bigB = 0;
+for (let a = 0; a < numbaers.length; a++) {
+const currb = numbaers[a];
+if (currb > bigB) {
+  bigB = currb;
+
+}
+}
+return bigB;
+}
+console.log(`That more big number`, biggerNumber()); */
+
+/* function someOfNumber() {
+  const number = [];
+  let n;
+  do {
+    n = parseInt(prompt(`pls write what y want number`));
+    if (!isNaN(n)){
+      number.push(n);
+    }
+  } while(!isNaN(n));
+let bigB = 0;
+for (let a = 0; a < numbaers.length; a++) {
+const currb = numbaers[a];
+if (currb > bigB) {
+  bigB = currb;
+
+}
+}
+return bigB;
+}
+console.log(`That more big number`, someOfNumber()); */
+
+/* const str = "hello";
+
+const arr = str.split("");
+arr.reverse();
+console.log(arr.join(" ")); */
+
+
+/* let arr = [26, 25];
+let copyArr = arr;
+console.log(arr); */
+
+
+/* function nameOf (arr) {
+  let count = 0;
+  while (arr.includes('Vova')) {
+    count ++;
+    }
+    return nameOf;
+  }
+  console.log(nameOf('Vova', 'Vova', 'Vika', 'Vlada')); */
+
+  /* function getPair1(arr1) {
+  const arr1 = getFirstPair[];
+  const getPair1 = [students[0], students[2]];
+  return getPair1;
+}
+console.log(getFirstPair());
+
+function getSecondPair(arr2) {
+  const getSecondPair = [students[1], students[3]];
+  return getSecondPair;
+}
+console.log(getSecondPair());
+
+function getThirdPair(arr3) {
+  const getThirdPair = [students[4], students[5]];
+  return getThirdPair;
+}
+console.log(getThirdPair());
+
+const pairStudents = [].concat([arr1], [arr2], [arr3]);
+console.log('pairStudents: ', pairStudents); */
